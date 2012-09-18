20a5113f60b3f94566df8c1966c829687032a589
Thomas Leyer <tole@blahlalla.de>
2012-09-19 00:33:31 +0200

    Works up to make install - not final

--- manifests/default.pp
+++ manifests/default.pp
@@ -3,7 +3,7 @@ class xymon {
 		ensure => present,
 	}
 
-	package { ["pcre", "pcre-devel"]:
+	package { ["pcre", "pcre-devel","httpd"]:
 		ensure => installed,
 	}
 
@@ -18,10 +18,38 @@ class xymon {
 	}
 
 	exec { "untar XYMON":
-		cwd     => "/tmp",
-		command => "/bin/tar xvzf /tmp/xymon_latest.tar.gz --strip-components=1 --directory /tmp/xymon_latest",
-		creates => "/tmp/xymon_latest",
+		cwd        => "/tmp/xymon_latest",
+		command    => "/bin/tar xvzf /tmp/xymon_latest.tar.gz --strip-components=1 --directory /tmp/xymon_latest",
+		creates    => "/tmp/xymon_latest/configure.server",
+		logoutput  => true,
 	}
+
+	exec { "XYMON configure.server":
+		cwd			    => "/tmp/xymon_latest",
+		command     => "/tmp/xymon_latest/configure.server --prefix=/tmp/xymon",
+		environment => ['USEXYMONPING=yes','ENABLESSL=y'],
+		require     => Exec["untar XYMON"],
+		creates     => "/tmp/xymon_latest/Makefile",
+		logoutput   => true,
+	}
+
+	exec { "XYMON make":
+		cwd         => "/tmp/xymon_latest",
+    command     => "/usr/bin/make",
+    require     => Exec["XYMON configure.server"],
+		creates     => "/tmp/xymon_latest/xymond/xymond", 
+    logoutput   => true,
+		timeout     => 0,
+  }
+
+	exec { "XYMON make install":
+		cwd         => "/tmp/xymon_latest",
+    command     => "/usr/bin/make install",
+    require     => Exec["XYMON make"],
+		creates     => "/tmp/xymon",
+    logoutput   => true,
+  }
+
 }
 
 include xymon
